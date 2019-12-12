e73c7f6c6be2bda4208e02d8054050b70e6541e8
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-12-12 13:32:55 -0500

    project setup

---
+++ src/firebase/operations.js
@@ -0,0 +1,27 @@
+import firebase from "./init";
+
+
+export const uploadImage = image => {
+  return new Promise(resolve => {
+    const storage = firebase.storage().ref();
+    const fileRef = storage.child(`images/${image.name}`);
+
+    fileRef.put(image).then(snapshot => {
+      console.log("snapshot.downloadURL", snapshot.downloadURL)
+      resolve(snapshot.downloadURL)
+    });
+  })
+}
+
+export const uploadFile = file => {
+  return new Promise(resolve => {
+    const storage = firebase.storage().ref();
+    const fileRef = storage.child(`files/${file.name}`);
+
+    fileRef.put(file).then(snapshot => {
+      console.log("snapshot.downloadURL", snapshot.downloadURL)
+      resolve(snapshot.downloadURL)
+    });
+  })
+}
+
